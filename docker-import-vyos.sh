de82556e93c692b8293997ce46636a823eeea999
KAH HOWE WOO <46815334+JustAyce@users.noreply.github.com>
2022-10-16 15:45:11 +0800

    Update docker-import-vyos.sh

--- docker-import-vyos.sh
+++ docker-import-vyos.sh
@@ -1,15 +1,15 @@
-mkdir vyos && cd vyos
+mkdir ~/vyos && cd ~/vyos
 wget https://s3-us.vyos.io/rolling/current/vyos-1.4-rolling-202209260217-amd64.iso
-mkdir rootfs
+mkdir ~/vyos/rootfs
 sudo chmod 777 vyos-1.4-rolling-202209260217-amd64.iso
 sudo mount -o loop vyos-1.4-rolling-202209260217-amd64.iso rootfs
 sudo apt-get install -y squashfs-tools
-mkdir unsquashfs
+mkdir ~/vyos/unsquashfs
 sudo unsquashfs -f -d unsquashfs/ rootfs/live/filesystem.squashfs
 # sudo tar -C unsquashfs -c . | sudo docker import - vyos:1.4-rolling-202209260217
 sudo tar -zcvf test.tar.gz unsquashfs && sudo docker import test.tar.gz vyos:1.4-rolling-202111281249
 sudo umount rootfs
-cd ..
-sudo rm -rf vyos
+cd ~/
+# sudo rm -rf vyos
 sudo docker run -d --rm --name vyos --privileged -v /lib/modules:/lib/modules vyos:1.4-rolling-202209260217 /sbin/init
 sudo docker exec -ti vyos su - vyos
